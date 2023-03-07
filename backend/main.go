2376268081cb96a028ee491bea1471fd2ed1f7de
jamesmern <jamesjo2019@gmail.com>
2023-03-08 01:42:16 +0900

    feat: update backend code

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,52 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type fizzbuzzBody struct {
+	Count int `json:"count" binding:"required"`
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-	  c.String(200, "pong")
+	r.POST("/fizzbuzz", func(c *gin.Context) {
+		var reqBody fizzbuzzBody
+
+		// Bind the request body to the User struct
+		if err := c.BindJSON(&reqBody); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			  	"message": "count parameter is required",
+				"status": 500,
+			})
+
+			return
+		}
+
+		if reqBody.Count < 0 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "count parameter is under 0",
+				"status": 500,
+			})
+
+			return
+		}
+
+		var returnMsg = ""
+		if reqBody.Count % 15 == 0 {
+			returnMsg = "FizzBuzz"
+		} else if reqBody.Count % 5 == 0 {
+			returnMsg = "FizzBuzz"
+		} else if reqBody.Count % 3 == 0 {
+			returnMsg = "FizzBuzz"
+		}
+
+		c.JSON(200, gin.H {
+			"status": 200,
+			"message": returnMsg,
+		})
 	})
 
 	return r
@@ -14,5 +54,5 @@ func setupRouter() *gin.Engine {
 
 func main() {
 	r := setupRouter()
-	r.Run(":8080")
+	r.Run(":4000")
 }
